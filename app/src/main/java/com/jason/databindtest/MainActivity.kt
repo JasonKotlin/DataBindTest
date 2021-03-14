a1c96f90dd736f65a85caeae7dddc3373a298c72
JasonYang <jason.yang@mitake.com.tw>
2021-03-15 00:43:25 +0800

    修改成有註解的範例重新上傳

--- app/src/main/java/com/jason/databindtest/MainActivity.kt
+++ app/src/main/java/com/jason/databindtest/MainActivity.kt
@@ -8,16 +8,46 @@ import com.jason.databindtest.databinding.ActivityMainBinding
 import com.jason.databindtest.viewModel.ActivityViewModel
 
 class MainActivity : AppCompatActivity() {
-    lateinit var mBinding: ActivityMainBinding
+    //因為後面可能還要用到，所以必須把 dataBinding 宣告成全域
+    lateinit var dataBinding: ActivityMainBinding
+    //viewModel必須先抓出來才有辦法使用裡面的東西所以也宣告成全域
+    //因為不是要一直去 new 他，所以要使用  viewMode 的用法
+    //by = 初始化的意思
+    //TODO by viewModels 背起來
     val mViewModel by viewModels<ActivityViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        mBinding.apply {
+        dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        //通常只會用到 apply 或是 let
+        dataBinding.apply {
+            //為了可以連動，所以必須要這樣寫
             lifecycleOwner = this@MainActivity
             viewmodel = mViewModel
         }
-        setContentView(mBinding.root)
+
+        //let，用法再自己去查
+        dataBinding.let {
+
+        }
+
+        setContentView(dataBinding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        setupView()
+    }
+
+    private fun setupView() {
+        dataBinding.babyTextTitle.apply {
+            background =
+                if (mViewModel.isShow.value == true) {
+                    resources.getDrawable(R.drawable.ic_launcher_background)
+                } else {
+                    resources.getDrawable(R.drawable.ic_launcher_foreground)
+                }
+        }
     }
 }
--- app/src/main/java/com/jason/databindtest/viewModel/ActivityViewModel.kt
+++ app/src/main/java/com/jason/databindtest/viewModel/ActivityViewModel.kt
@@ -4,9 +4,18 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ActivityViewModel: ViewModel() {
-    var binding_test_txt = MutableLiveData("GGG")
+    //如果要有Default值，必須使用 MutableLiveData
+    //幾乎都是用 MutableLiveData，背起來吧
+    val mBindingText = MutableLiveData("GG")
+    val isShow = MutableLiveData(true)
 
-    fun btnChangeText(){
-        binding_test_txt.value = "be Onclick"
+    fun btn1Click(){
+        mBindingText.value += 1
+        isShow.value = true
+    }
+
+    fun btn2Click(){
+        //因為他是 lifeData 不是真正的 String 所以必須是 .value 再設值
+        isShow.value = false
     }
 }
